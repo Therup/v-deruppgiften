c06f4b445e7380f79f36641ad2d6ae157f33bd90
marcus therup <m_therup@hotmail.com>
2022-11-15 09:56:53 +0100

    Add script

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function goWeather(){ //funktion som hämtar kordinater och sedan kör vädret efter stad
+    let city = document.getElementById("msg").value; //Plockar ut det man skriver in i textrutan
+    let cities = city; //Lägger in det man skriver i cities variablen
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cities}&limit=5&appid=d7742897b3cfa5f0b723ebb8047a69bf`) // Hämtar info om stad
+         .then(response => {
+            if(response.ok) {
+           return response.json(); //Konverterar informationen        
+       } 
+    })
+        .then(name => { 
+             console.log(name);
+             let lat = `${name[0].lat}`;//Plockar ut latitud och ger en variabel
+             let lon = `${name[0].lon}`;//Plockar ut longitud och ger en variabel
+             document.getElementById("city").innerHTML=`The local weather in ${name[0].name}, ${name[0].country}` //Lägger in stad och land i html sidan
+             fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=d7742897b3cfa5f0b723ebb8047a69bf`) //Hämtar data om väder för vald stad
+             .then(weather => {
+                 return weather.json() //Konverterar informationen  
+                  
+            })
+            .then(dataWeather => {   
+                console.log(dataWeather.main.temp);
+                console.log(dataWeather.weather[0].description);
+                const removedDecimal = Math.round(dataWeather.main.temp); //Rundar av decimaler från temperaturen
+                document.getElementById("weather").innerHTML=`${removedDecimal}C with ${dataWeather.weather[0].description}` //Lägger in temperatur och väder info
+                
+            })
+           
+         });
+  
+        };
+
+
